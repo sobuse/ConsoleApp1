3533c6f48901c9f21c67df3b1662023267961241
sammy <ehuerikenbaba@gmail.com>
2021-07-05 11:34:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+               
+                Console.WriteLine("@Echo: " + input);
+            }
+        }
+    }
+}
